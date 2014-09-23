0d11d912fcd5fdbbf7e2ef6ad0d0b4ca67398462
Nino Müller <n.mueller@substring.ch>
2014-09-23 10:41:36 +0200

    Notwendige Options hinzugefügt fürs CLI

--- lib/yarpler/cli.rb
+++ lib/yarpler/cli.rb
@@ -5,13 +5,31 @@ module Yarpler
   class CLI < Thor
     class_option :verbose, :type => :boolean
 
-    desc "hello NAME", "say hello to NAME"
-    def hello(name)
-      puts "Hello #{name}"
-      puts "more text" if options[:verbose]
+    #desc "hello NAME", "say hello to NAME"
+    #def hello(name)
+    #  puts "Hello #{name}"
+    #  puts "more text" if options[:verbose]
+    #end
+
+    desc "display FILE", "Display the complete input file (yai and yad)"
+    def display(file)
+      yarpler = Yarpler::Runner.new
+      yarpler.display(file)
     end
 
-    desc "parse FILE", "parse yarpler"
+    desc "tree FILE", "Display the AST of the input file"
+    def tree(file)
+      yarpler = Yarpler::Runner.new
+      yarpler.tree(file)
+    end
+
+    desc "flat FILE", "Return a flat input file in YARPL"
+    def flat(file)
+      yarpler = Yarpler::Runner.new
+      yarpler.flat(file)
+    end
+
+    desc "parse FILE", "Parse input file and convert to MiniZinc"
     def parse(file)
       yarpler = Yarpler::Runner.new
       yarpler.run(file)
--- lib/yarpler/runner.rb
+++ lib/yarpler/runner.rb
@@ -4,13 +4,39 @@
 module Yarpler
   class Runner
 
-    def run(filename)
+    ##
+    # Displays the file
+    #
+    def display(filename)
       file = Yarpler::Utils::FileLoader.new(filename)
-      #file.print
-
-      parser = Yarpler::Parser.new(file.get_content)
-      parser.print
+      file.print
     end
 
+    ##
+    # Displays the AST
+    #
+    def tree(filename)
+      file = Yarpler::Utils::FileLoader.new(filename)
+      parser = Yarpler::Parser.new(file.get_content)
+      parser.print
+      puts "Not implemented yet."
+    end
+
+    ##
+    # Converts a file to flattened YARPLER code
+    #
+    def flat(filename)
+      puts "Not implemented yet."
+    end
+
+    ##
+    # Converts a file to MiniZinc
+    #
+    def run(filename)
+      file = Yarpler::Utils::FileLoader.new(filename)
+      parser = Yarpler::Parser.new(file.get_content)
+      parser.print
+      puts "Not implemented yet."
+    end
   end
 end
