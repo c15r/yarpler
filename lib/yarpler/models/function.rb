6af9f0b761d2860d7b01d532c87357b18b41314c
Christoph Schnyder <christoph.schnyder@gmail.com>
2014-10-05 17:12:13 +0200

    Split expressions into function expressions

---
+++ lib/yarpler/models/function.rb
@@ -0,0 +1,20 @@
+module Yarpler
+  module Models
+    class Function
+
+      def initialize
+
+      end
+    end
+
+    class CountFunction < Function
+
+      attr_accessor :element
+      attr_accessor :range
+
+      def initialize
+
+      end
+    end
+  end
+end
