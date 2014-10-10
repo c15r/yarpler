d78d9e35747f6d5bb22377132a8a0fc91399f3f9
Christoph Schnyder <christoph.schnyder@gmail.com>
2014-10-10 14:37:44 +0200

    Clean rubocop violations in file_loader.rb

--- lib/yarpler/utils/file_loader.rb
+++ lib/yarpler/utils/file_loader.rb
@@ -1,31 +1,38 @@
 module Yarpler
   module Utils
+    ##
+    # YARPL File Loader is responsible to read the problem files
+    # and create a string out of them
+    #
+    # YARPL files can be splitted into two different files:
+    # * .yad, the yad file contains the model declaration of the problem
+    # * .yai, the yai file holds the model instantiation and the constraint
+    # declaration
+    #
+    # It's optional to split the problem into multiple files.
+    # You can pack all just in one .yai file.
     class FileLoader
+      attr_reader :content
+
       def initialize(filename)
-        @contents = ''
+        @content = ''
         @filename = filename
         load_files
-      end
-
-      def get_content
-        @contents
       end
 
       private
 
       def load_files
         filename_domain = @filename.sub('.yai', '.yad')
-        if File.file?(@filename) # @TODO Melde Fehler sobald File nicht vorhanden.
-          if File.file?(filename_domain)
-            load_file_to_contents(filename_domain)
-          end
-          load_file_to_contents(@filename)
-        end
+        load_file_to_content(filename_domain)
+        load_file_to_content(@filename)
       end
 
-      def load_file_to_contents(filename)
+      def load_file_to_content(filename)
+        # @TODO Melde Fehler sobald File nicht vorhanden.
+        return false if File.file?(filename_domain)
         file = File.new(filename, 'r')
-        @contents << file.read
+        @content << file.read
         file.close
       end
     end
