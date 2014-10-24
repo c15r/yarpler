5855e2188823d10ac8719156d285673e50227922
Christoph Schnyder <christoph.schnyder@gmail.com>
2014-10-24 11:45:20 +0200

    Fix violations in cli.rb

--- lib/yarpler/cli.rb
+++ lib/yarpler/cli.rb
@@ -1,15 +1,15 @@
 module Yarpler
+  # Command Line Interface of YARPLER
   class CLI < Thor
-
     def initialize(*args)
       super
       Yarpler::Log.instance.switch_level(:debug)
     end
 
     desc 'solve [FILE] [TRANSLATORS]', 'Parse input file and solve it'
-    option :display_ast, :type => :boolean, :aliases => :ast
-    option :display_model, :type => :boolean, aliases: :model
-    option :no_result, :type => :boolean
+    option :display_ast, type: :boolean, aliases: :ast
+    option :display_model, type: :boolean, aliases: :model
+    option :no_result, type: :boolean
     def solve(file, *args)
       core = Yarpler::Core.new
       solution = core.solve_from_file(file, args)
@@ -23,6 +23,5 @@ module Yarpler
       printer.print_ast solution.ast if options[:display_ast]
       printer.print_model solution.model if options[:display_model]
     end
-
   end
 end
