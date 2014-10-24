6383e90e6a7430f80a50b8ce3a49bef5c6b33e78
Christoph Schnyder <christoph.schnyder@gmail.com>
2014-10-24 15:30:32 +0200

    Fix violations for field_accessor_interpreter.rb

--- lib/yarpler/interpreters/field_accessor_interpreter.rb
+++ lib/yarpler/interpreters/field_accessor_interpreter.rb
@@ -1,11 +1,12 @@
 module Yarpler
   module Interpreters
+    # FieldAccessorInterpreter is responsible for interpreting statements which access fields of objects
     class FieldAccessorInterpreter
       attr_accessor :field
 
       def initialize(item)
         if item.size == 1
-          @field=InstanceInterpreter.new(item).instance
+          @field = InstanceInterpreter.new(item).instance
         else
           process_field(item)
         end
@@ -13,16 +14,28 @@ module Yarpler
 
       def process_field(item)
         if item.size == 3
-          # @TODO Funktioniert erst mit dreier-Verschachtelung, muss natürlich für alles funktionieren
+          # TODO: Funktioniert erst mit dreier Verschachtelungen
           @field = Yarpler::Models::Field.new
           @field.variable = Yarpler::Models::Substitute.new
           @field.variable.variable = item[0].to_s
           @field.variable.attribute = item[1].to_s
-          @field.attribute = item[item.size-1].to_s
+          @field.attribute = item[item.size - 1].to_s
         else
           process_single(item)
         end
       end
+
+      # def substitute(item)
+      #   problem = Yarpler::Models::Problem.instance
+      #   instance = problem.objects[item[0].to_s]
+      #   if instance.nil?
+      #   else
+      #     obj=.get_value(item[1].to_s)
+      #     item[1]=obj.get_instance_name.to_s
+      #   end
+      #
+      #   item[1..item.size]
+      # end
 
       def process_single(item)
         @field = Yarpler::Models::Field.new
