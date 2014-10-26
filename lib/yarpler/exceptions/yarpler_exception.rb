97e6014e7458a43a4810c589dcde3782db0d2fc6
c15r <christoph.schnyder@gmail.com>
2014-10-26 22:57:21 +0100

    Fix violations in yarpler_exception

--- lib/yarpler/exceptions/yarpler_exception.rb
+++ lib/yarpler/exceptions/yarpler_exception.rb
@@ -1,31 +1,34 @@
 module Yarpler
   module Exceptions
+    # Main YARPLER Exception
+    # Derive all specific exceptions from this one
     class YarplerException < StandardError
       attr_reader :message
 
-      def initialize()
+      def initialize
         @message = 'Unknown Exception'
       end
-
     end
 
+    # Indicates that a class could not be found
     class ClassNotInModelException < YarplerException
       def initialize(name)
-        @message = "The class [%s] has not been defined in the model." % [name]
+        @message = "The class #{name} has not been defined in the model."
       end
     end
 
+    # Indicates that there was a syntax error in a YARPL file
     class SyntaxErrorException < YarplerException
-      def initialize(addidional)
-        @message = 'Syntax error in YARPL input file at ' + addidional
+      def initialize(additional)
+        @message = "Syntax error in YARPL input file at #{additional}"
       end
     end
 
+    # Indicates that there was a constant used without initialization
     class UninitializedConstantException < YarplerException
       def initialize(instance, attribute)
-        @message = "The constant [%s.%s] has not been initialized." % [instance,attribute]
+        @message = "The constant [#{instance}.#{attribute}] has not been initialized."
       end
     end
-
-    end
+  end
 end
