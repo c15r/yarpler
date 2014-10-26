c79004379a951ae211421f259cc799676f891a15
c15r <christoph.schnyder@gmail.com>
2014-10-26 11:54:09 +0100

    Elaborate on class documentation of the SetInterpreter

--- lib/yarpler/interpreters/set_interpreter.rb
+++ lib/yarpler/interpreters/set_interpreter.rb
@@ -1,5 +1,11 @@
 module Yarpler
   module Interpreters
+    # SetInterpreter processes a YARPL Set and pushes the values to an internal data structure
+    #
+    # == YARPL Example
+    #
+    #   {d1,d2,f}
+    #
     class SetInterpreter
       attr_accessor :set
 
