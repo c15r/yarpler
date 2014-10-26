83911fbd25f18cf09f129e300c72cc8df4487538
c15r <christoph.schnyder@gmail.com>
2014-10-26 11:41:32 +0100

    Fix rubocop violations in solve_interpreter.rb

--- lib/yarpler/interpreters/solve_interpreter.rb
+++ lib/yarpler/interpreters/solve_interpreter.rb
@@ -16,20 +16,42 @@ module Yarpler
 
       def tree_converter(tree)
         tree.each do |item|
-          case item.to_s
-            when 'CONSTRAINT_DECLARATION'
-              constraint_interpreter = ConstraintInterpreter.new(item, @objects)
-              @constraints.push(constraint_interpreter.constraint)
-            when 'SATISFY'
-              @solve.statement = item.to_s
-            when 'MINIMIZE'
-              @solve.statement = item.to_s
-              @solve.expression = ExpressionInterpreter.new(item[0]).expression
-            when 'MAXIMIZE'
-              @solve.statement = item.to_s
-              @solve.expression = ExpressionInterpreter.new(item[0]).expression
-          end
+          interpret_statement(item)
         end
+      end
+
+      private
+
+      def interpret_statement(statement)
+        case statement.to_s
+          when 'CONSTRAINT_DECLARATION'
+            interpret_constraint(statement)
+          when 'SATISFY'
+            interpret_satisfy(statement)
+          when 'MINIMIZE'
+            interpret_minimize(statement)
+          when 'MAXIMIZE'
+            interpret_maximize(statement)
+        end
+      end
+
+      def interpret_constraint(constraint)
+        constraint_interpreter = ConstraintInterpreter.new(constraint, @objects)
+        @constraints.push(constraint_interpreter.constraint)
+      end
+
+      def interpret_satisfy(statement)
+        @solve.statement = statement.to_s
+      end
+
+      def interpret_minimize(statement)
+        @solve.statement = statement.to_s
+        @solve.expression = ExpressionInterpreter.new(statement[0]).expression
+      end
+
+      def interpret_maximize(statement)
+        @solve.statement = statement.to_s
+        @solve.expression = ExpressionInterpreter.new(statement[0]).expression
       end
     end
   end
